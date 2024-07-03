c8ba0d48d0cc3fa3452e35eb7a4ff7464b559d13
RyadC <r.chair@hotmail.fr>
2024-07-03 16:52:38 +0200

    feat: add all activity routes

--- app/routers/activity.router.js
+++ app/routers/activity.router.js
@@ -4,8 +4,24 @@ import { Router } from "express";
 
 const activityRouter = Router();
 
+// TODO: Code activityController module
+
 activityRouter.route('/')
   .post(activityController.store);
+
+activityRouter.route('/:id')
+  .patch(activityController.update)
+  .delete(activityController.destroy);
+
+activityRouter.route('/:country/:city')
+  .get(activityController.index);
+
+activityRouter.route('/rating')
+  .get(activityController.index);
+
+
+activityRouter.route('/recent')
+  .get(activityController.index);
 
 
 export default activityRouter;
