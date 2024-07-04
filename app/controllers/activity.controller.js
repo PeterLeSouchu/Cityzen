b92b27a58ab530c607cc1d629882e3ed7ed65d2b
RyadC <r.chair@hotmail.fr>
2024-07-04 05:48:56 +0200

    feat: add core controller && integration test for some activity routes

---
+++ app/controllers/activity.controller.js
@@ -0,0 +1,27 @@
+
+
+const activityController = {
+  index(req, res) {
+
+  },
+  
+  show(req, res) {
+
+  },
+
+  store(req, res) {
+
+  },
+
+  update(req, res) {
+
+  },
+
+  destroy(req, res) {
+
+  },
+
+
+}
+
+export default activityController;
