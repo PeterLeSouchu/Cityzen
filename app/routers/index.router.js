b0655ce818145ab5951b6be9c45a82660fbd3f54
RyadC <r.chair@hotmail.fr>
2024-07-04 08:57:28 +0200

    fix: add main signin route in index router

--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -4,6 +4,7 @@ import { Router } from "express";
 // EXTERNAL MODULES
 import activityRouter from "./activity.router.js";
 import favoriteRouter from "./favorite.router.js";
+import signinRouter from "./signin.router.js";
 
 
 
@@ -11,6 +12,7 @@ const router = Router();
 
 router.use('/activity', activityRouter);
 router.use('/favorite', favoriteRouter);
+router.use('/signin', signinRouter)
 
 // TODO : Handler error middleware here 👇
 
