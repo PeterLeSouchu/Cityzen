4011fdb6f2ea69b67ccefe101748c87b0f395174
RyadC <r.chair@hotmail.fr>
2024-07-05 15:27:11 +0200

    fix: install joi package

--- app/controllers/signup.controller.js
+++ app/controllers/signup.controller.js
@@ -1,8 +1,11 @@
+import otpGenerator from "otp-generator";
 
 const signupController = {
 
   sendOTP(req, res) {
+    const OTP = otpGenerator.generate(6);
     console.log(req.body);
+    console.log(OTP);
   },
 
   checkUserByOTP(req, res) {
