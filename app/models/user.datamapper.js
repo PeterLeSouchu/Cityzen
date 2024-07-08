066ad93ec4c71bb5728a5982dbbe02609e1ecb0c
RyadC <r.chair@hotmail.fr>
2024-07-08 07:20:58 +0200

    feat: add returning clause in datamapper

--- app/models/user.datamapper.js
+++ app/models/user.datamapper.js
@@ -1,12 +1,15 @@
+// EXTERNAL MODULES
+import client from "../config/pg.client.js";
 
 
 const userDatamapper = {
-  async save(email, hash) {
+  async save(email, hash, pseudo) {
 
     const response = await client.query(`
-      INSERT INTO "user"("email", "password")
-        VALUES ($1, $2)
-      ;`, [email, hash])
+      INSERT INTO "user"("email", "password", "pseudo")
+        VALUES ($1, $2, $3)
+      RETURNING *
+      ;`, [email, hash, pseudo])
     ;
 
     console.log(response);
