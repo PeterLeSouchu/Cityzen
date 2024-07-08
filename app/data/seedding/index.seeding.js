db67e039ef3da1cc4ffc95658a9c007ede051bea
RyadC <r.chair@hotmail.fr>
2024-07-08 18:09:53 +0200

    fix: comments

--- app/data/seedding/index.seeding.js
+++ app/data/seedding/index.seeding.js
@@ -10,26 +10,26 @@ import { getOneUser, insertUser } from "./user.seeding.js";
 
 const API_DEP_URL = 'https://geo.api.gouv.fr/departements';
 
-// // 1- Insert all countries into DB
-// await insertCountries(client, countries);
-// console.log('Countries inserted');
+// 1- Insert all countries into DB
+await insertCountries(client, countries);
+console.log('Countries inserted');
 
-// // 2- Insert all departments into DB
-// const allDepartmentsFromAPI = await fetchDepartments(API_DEP_URL);
-// await insertDepartments(client, allDepartmentsFromAPI);
-// console.log('Departments inserted');
+// 2- Insert all departments into DB
+const allDepartmentsFromAPI = await fetchDepartments(API_DEP_URL);
+await insertDepartments(client, allDepartmentsFromAPI);
+console.log('Departments inserted');
 
-// // 3- Insert all cities from one department into DB
-// const country = await getCountryFromDB(client, 'France');
-// const allDepartments = await getDepartmentsFromDB(client);
+// 3- Insert all cities from one department into DB
+const country = await getCountryFromDB(client, 'France');
+const allDepartments = await getDepartmentsFromDB(client);
 
-// for (const department of allDepartments) {
-//   const citiesFromDepartment = await fetchCitiesFromDepartement(department.code);
-//   await insertCities(client, country, department, citiesFromDepartment);
-// }
+for (const department of allDepartments) {
+  const citiesFromDepartment = await fetchCitiesFromDepartement(department.code);
+  await insertCities(client, country, department, citiesFromDepartment);
+}
 
-// // 4- Insert user
-// await insertUser(client, "cityzen@cityzen.com", "Cityzen.1", "Cityzen");
+// 4- Insert user
+await insertUser(client, "cityzen@cityzen.com", "Cityzen.1", "Cityzen");
 
 // 5 - Insert activities
 const cityToSearch = 'Perpignan';
