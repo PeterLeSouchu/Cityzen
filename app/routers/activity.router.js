850feb0291c717569e3a808857a1e79ab9cff6c6
RyadC <r.chair@hotmail.fr>
2024-07-16 16:18:52 +0200

    feat: add error handler on controller && fix: imort ApiError in datamapper

--- app/routers/activity.router.js
+++ app/routers/activity.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import activityController from "../controllers/activity.controller.js";
+import catchHandlerController from "../middlewares/error-handler.middleware.js";
 
 // import validationMiddleware from '../../middlewares/validation.middleware.js';
 
@@ -7,17 +8,25 @@ const activityRouter = Router();
 
 // Route pour obtenir les activités récentes
 activityRouter.route('/recent')
-  .get(activityController.showRecent);
+  .get(
+    catchHandlerController(activityController.showRecent)
+  );
 
 activityRouter.route('/rating')
-  .get(activityController.showRating);
+  .get(
+    catchHandlerController(activityController.showRating)
+  );
 
 // Route pour obtenir les activités d'une ville et d'un pays spécifiques
 activityRouter.route('/:country/:city')
-  .get(activityController.index);
+  .get(
+    catchHandlerController(activityController.index)
+  );
 
 // Route pour obtenir une activité par son ID
 activityRouter.route('/:id')
-  .get(activityController.show);
+  .get(
+    catchHandlerController(activityController.show)
+  );
 
 export default activityRouter;
