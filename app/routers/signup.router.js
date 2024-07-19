a4cd7cbd6c7a599aaafdebce45548ff89b7d166d
RyadC <r.chair@hotmail.fr>
2024-07-19 07:24:07 +0200

    fix: authentication validation on signup

--- app/routers/signup.router.js
+++ app/routers/signup.router.js
@@ -5,15 +5,16 @@ import { Router } from "express";
 import signupController from "../controllers/signup.controller.js";
 import validationSchema from "../schema-validations/validation.schema.js";
 import signupAuthenticationSchema from "../schema-validations/signup/signup.authentication.schema.js";
+import catchHandlerController from "../middlewares/error-handler.middleware.js";
 
 const signupRouter = Router();
 
 signupRouter.route('/')
   // For check user identity with his email and password we send an OTP code by email
-  .post(signupController.sendOTP)
+  .post(validationSchema(signupAuthenticationSchema, 'body', undefined, false), catchHandlerController(signupController.sendOTP))
 
 signupRouter.route('/confirmation')
   // For check the received OTP code and signup the user in DB if ok
-  .post(signupController.checkUserByOTP);
+  .post(validationSchema(signupAuthenticationSchema, 'body', undefined, false), catchHandlerController(signupController.checkUserByOTP));
   
 export default signupRouter;
