45836c5608b232d566e8b9e55022e975ed27e79b
PeterLeSouchu <peter22510@gmail.com>
2024-08-18 19:31:46 +0200

    feat: add mail send

--- app/controllers/signup.controller.js
+++ app/controllers/signup.controller.js
@@ -1,14 +1,15 @@
 // TIERCE MODULES
 import 'dotenv/config';
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 import otpGenerator from 'otp-generator';
 
 // EXTERNAL MODULES
 import userDatamapper from '../models/user.datamapper.js';
+import nodemailer from 'nodemailer';
 import { hashPassword } from '../utils/bcrypt.js';
-import sendMail from '../utils/send-mail.js';
-import transporter from '../config/transporter.config.js';
-import generateHTMLMail from '../config/html-mail.config.js';
+// import sendMail from '../utils/send-mail.js';
+// import transporter from '../config/transporter.config.js';
+// import generateHTMLMail from '../config/html-mail.config.js';
 import errors from '../errors/errors.js';
 import ApiError from '../errors/api.error.js';
 
@@ -43,7 +44,33 @@ const signupController = {
       const OTP = otpGenerator.generate(6);
       console.log(OTP);
 
-      const htmlText = generateHTMLMail(pseudo, OTP);
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      await transporter.sendMail({
+        from: {
+          name: 'Cityzen Team',
+          address: 'cityzen688@gmail.com',
+        },
+        to: `${email}`, // list of receivers
+        subject: 'OTP Code', // Subject line
+        html: `<h1> CityZen </h1>
+            <p>Bonjour ${pseudo},</p>
+            <p>Nous vous souhaitons la bienvenue chez CityZen! </p>
+            <p>Pour valider votre inscription, il ne vous suffit plus qu'à renseigner le code suivant sur la plateforme: <span> ${OTP}</span></p>
+            <p>À tout de suite !</p>
+          `,
+      });
+
+      // const htmlText = generateHTMLMail(pseudo, OTP);
 
       // sendMail(transporter, htmlText);
 
